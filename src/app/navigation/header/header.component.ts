035099d6b9355aeecedc4fd3d9870a0de49b81a8
yanosz <github@yanosz.net>
2019-05-31 18:18:40 +0200

    Fix build error

--- src/app/navigation/header/header.component.ts
+++ src/app/navigation/header/header.component.ts
@@ -27,7 +27,7 @@ export class HeaderComponent implements OnInit, OnDestroy {
 
   private rotateRefreshButtonSubscription: Subscription;
   private matMenuSubscription: Subscription;
-  private matMenu: any;
+  matMenu: any;
 
   constructor(private headerBarService: HeaderBarService) { }
 

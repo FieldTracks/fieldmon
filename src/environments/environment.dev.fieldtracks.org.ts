ed316664725f38bedd1d622cd581ef1fcac6db41
yanosz <github@yanosz.net>
2019-07-03 18:46:20 +0200

    Added envs for hosted environments

---
+++ src/environments/environment.dev.fieldtracks.org.ts
@@ -0,0 +1,7 @@
+export const environment = {
+  production: true,
+  mqtt_broker: 'dev.fieldtracks.org',
+  mqtt_port: 8882,
+  mqtt_path: '',
+  uuid: 'fd:a5:06:93:a4:e2:4f:b1:af:cf:c6:eb:07:64:78:25'
+};

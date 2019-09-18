f2c035c61e1861c6622ad74bb77567b96baae157
yanosz <github@yanosz.net>
2019-09-18 18:29:37 +0200

    Fix build error

--- src/environments/environment.test.fieldtracks.org.ts
+++ src/environments/environment.test.fieldtracks.org.ts
@@ -3,5 +3,6 @@ export const environment = {
   mqtt_broker: 'test.fieldtracks.org',
   mqtt_port: 8883,
   mqtt_path: '',
-  uuid: 'fd:a5:06:93:a4:e2:4f:b1:af:cf:c6:eb:07:64:78:25'
+  uuid: 'fd:a5:06:93:a4:e2:4f:b1:af:cf:c6:eb:07:64:78:25',
+  grafana_base: 'https://test.fieldtracks.org/grafana/d/Stones/stones?orgId=1&var-node='
 };

95c13fadae3e6dda36c4391dfd856fb3045bea6a
yanosz <github@yanosz.net>
2019-09-24 20:24:31 +0200

    Added local.fieldtracks.org-environment

---
+++ src/environments/environment.local.fieldtracks.org.ts
@@ -0,0 +1,8 @@
+export const environment = {
+  production: true,
+  mqtt_broker: 'prod.fieldtracks.org',
+  mqtt_port: 8884,
+  mqtt_path: '',
+  uuid: 'fd:a5:06:93:a4:e2:4f:b1:af:cf:c6:eb:07:64:78:25',
+  grafana_base: 'https://prod.fieldtracks.org/grafana/d/Stones/stones?orgId=1&var-node='
+};

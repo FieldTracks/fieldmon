da397e4011b8e21854d06593d8bab3cc1f9c7993
yanosz <github@yanosz.net>
2020-08-13 16:12:38 +0200

    Added docker target

---
+++ src/environments/environment.docker.ts
@@ -0,0 +1,8 @@
+export const environment = {
+  production: true,
+  mqtt_broker: window.location.hostname,
+  mqtt_port: 8883,
+  mqtt_path: '',
+  uuid: 'fd:a5:06:93:a4:e2:4f:b1:af:cf:c6:eb:07:64:78:25',
+  grafana_base: window.location.protocol + '//' +  window.location.hostname + '/grafana/d/Stones/stones?orgId=1&var-node='
+};
